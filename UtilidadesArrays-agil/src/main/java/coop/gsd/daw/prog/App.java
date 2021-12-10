a57c692e30b53bc127c5e4f11ab00e78273dcdb3
a.gil <a.gil@2DAW-02.2SMR.GSD.local>
2021-12-10 13:24:41 +0100

    primer UtilidadesArrays

---
+++ UtilidadesArrays-agil/src/main/java/coop/gsd/daw/prog/App.java
@@ -0,0 +1,14 @@
+package coop.gsd.daw.prog;
+
+import java.util.Arrays;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+     int [] array = {1,31,4,-1,-5};
+     int num = 5;
+     UtilidadesArrays.media(array);
+   UtilidadesArrays.incrementarArray(array, num);
+    }
+}
