863b6211bb135af987ad6537b1081f0a8dd80e9a
a.gil <a.gil@2DAW-02.2SMR.GSD.local>
2021-12-10 13:23:39 +0100

    Primer SnakesandLadders

---
+++ SnakesAndLadders-agil/src/main/java/net/agil/programacion/SnakesAndLadders_agil/App.java
@@ -0,0 +1,14 @@
+package net.agil.programacion.SnakesAndLadders_agil;
+
+public class App {
+	public static void main(String[] args)
+
+	{
+		int posjugador1 = 1;
+		int posjugador2 = 1;
+
+		tablero2.turno(posjugador1, posjugador2);
+
+		
+	}
+}
